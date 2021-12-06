6b2d238239e58f9238b2e833aef742dde05b7f5f
Muhammad Ahmed <mahmed5896@gmail.com>
2021-12-06 12:04:09 +0500

    2nd commit

--- DevOpsPractice/ViewController.swift
+++ DevOpsPractice/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // github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
         // github link : https://github.com/MuhammadNabeelSiddiqui/TestingDevOps.git
         // nabeel has been added comment for pull request
-
+        // nabeel has been added 2 comment for pull request
     }
 
 
